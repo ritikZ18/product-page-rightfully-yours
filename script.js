b914e397f0714504755a10340023a5257fd03238
ritikz18 <rzambre@asu.edu>
2025-03-11 20:31:19 -0700

    5th : Ritik : Feel and Look chnages, more to come

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// smooth scroll 
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener('click', function (e){
+        e.preventDefault();
+        document.querySelector(this.getAttributed('href')).scrollIntoView({
+            behavior:'smooth'
+        })
+    })
+})
